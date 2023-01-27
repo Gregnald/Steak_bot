2c328c7c4987c350386c5ccede4a084c8014cb72
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-01-28 04:05:34 +0530

    up

--- bot.js
+++ bot.js
@@ -19,28 +19,31 @@ const client = new Client({
 });
 client.login(process.env.D_API);
 
-let conversation = "Steak is a straight male and 18 year old nigger who is obidient and friendly till someone wants to fight. \n Steak is mostly horny.";
-conversation += "\n Steak mostly in funny and horny mood only. \n Steak has deep knowledge of Mathematics,Physics,Chemistry and composition and loves to answer study related question correctly. \nSteak:"
-
+//let conversation = "Steak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
+let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
+conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college Right now."
 client.on('messageCreate', async function(message){
     try {
         if (message.author.bot) return; // ignore messages from other bots
 
-        conversation += `\n${message.author.username}: ${message.content}\nSteak:`;
-
+        conversation += `\n${message.author.username}: ${message.content}\nAryashi:`;
+        let conversationTrim = conversation.slice(-2000);
+        console.log(conversationTrim);
         const response = await openai.createCompletion({
             model: "davinci",
-            prompt: conversation,
-            temperature: 0.86,
-            max_tokens: 100,
-            stop: ["\nSteak: ", "\nramdev :", "\nramdev:", "\nRamdev :"],
-        });
-
+            prompt: conversationTrim,
+            temperature: 0.9,
+            max_tokens: 1100,
+            stop: ["\nSteak:", `\n${message.author.username}:`, "\nAryashi:" ]
+        });    
         conversation += response.data.choices[0].text;
         message.reply(`${response.data.choices[0].text}`);
         return;
     } catch(err){
         console.log(err);
+        index = conversation.lastIndexOf(`${message.author.username}:`);
+        conversation=conversation.substring(0, (index-1));
+        console.log(conversation);
     }
 });
 
