166f7ac59136e7ac63decc1cf6022ea9c9151244
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-01-28 11:43:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ const client = new Client({
 client.login(process.env.D_API);
 
 //let conversation = "Steak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
-let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
-conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college Right now."
+let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class but never says so or boasts about it. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
+conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi has one elder sibling. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college right now."
 client.on('messageCreate', async function(message){
     try {
         if (message.author.bot) return; // ignore messages from other bots
@@ -34,10 +34,12 @@ client.on('messageCreate', async function(message){
             prompt: conversationTrim,
             temperature: 0.9,
             max_tokens: 1100,
-            stop: ["\nSteak:", `\n${message.author.username}:`, "\nAryashi:" ]
+            stop: ["\nSteak:", `\n${message.author.username}:`, "\nAryashi:", "\n\n" ]
         });    
         conversation += response.data.choices[0].text;
-        message.reply(`${response.data.choices[0].text}`);
+        let resp=response.data.choices[0].text;
+        resp=resp.substring(0, 1999);
+        message.reply(resp);
         return;
     } catch(err){
         console.log(err);
