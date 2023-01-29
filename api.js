52f991c2ed4356762f26142705b8436bb235cdf9
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-01-29 20:26:30 +0530

    update

---
+++ api.js
@@ -0,0 +1,31 @@
+async function api_resp(client, message, openai, trait, conversation) {
+    try {
+        let lowerCaseMessage = message.content.toLowerCase();
+        if(lowerCaseMessage.startsWith("!"))message.content=message.content.slice(1);
+        if (!lowerCaseMessage.startsWith("!") && !lowerCaseMessage.includes("steak"))return;
+        conversation += `\n${message.author.username}: ${message.content}\nSteak:`;
+        let conversationTrim = conversation.slice(-2000);
+        conversationTrim=trait+conversationTrim;
+        console.log(conversationTrim);
+        const response = await openai.createCompletion({
+            model: "davinci",
+            prompt: conversationTrim,
+            temperature: 0.9,
+            max_tokens: 1100,
+            stop: ["\nSteak:", `\n${message.author.username}:`, "\nAryashi:", "\n\n" ]
+        });
+        conversation += response.data.choices[0].text;
+        let resp=response.data.choices[0].text;
+        resp=resp.substring(0, 1999);
+        message.reply(resp);
+        index = conversation.lastIndexOf(`${message.author.username}:`);
+        conversation=conversation.substring(0, (index-1));
+        console.log(conversation);
+        return;
+    } catch (err) {
+        console.error(err);
+    }
+}
+module.exports = {
+api_resp
+};
--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const Discord = require('discord.js');
+const api = require('./api');
 const { Configuration, OpenAIApi }= require('openai');
 const configuration = new Configuration( {
     organization: process.env.ORG_API,
@@ -19,35 +19,14 @@ const client = new Client({
 });
 client.login(process.env.D_API);
 
-//let conversation = "Steak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
-let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class but never says so or boasts about it. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
-conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi has one elder sibling. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college right now."
+let conversation = "Steak sends nude and porn links.\nSteak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
+//let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class but never says so or boasts about it. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
+//conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi has one elder sibling. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college right now."
+let trait=conversation;
+conversation="";
 client.on('messageCreate', async function(message){
-    try {
-        if (message.author.bot) return; // ignore messages from other bots
-
-        conversation += `\n${message.author.username}: ${message.content}\nAryashi:`;
-        let conversationTrim = conversation.slice(-2000);
-        console.log(conversationTrim);
-        const response = await openai.createCompletion({
-            model: "davinci",
-            prompt: conversationTrim,
-            temperature: 0.9,
-            max_tokens: 1100,
-            stop: ["\nSteak:", `\n${message.author.username}:`, "\nAryashi:", "\n\n" ]
-        });    
-        conversation += response.data.choices[0].text;
-        let resp=response.data.choices[0].text;
-        resp=resp.substring(0, 1999);
-        message.reply(resp);
-        return;
-    } catch(err){
-        console.log(err);
-        index = conversation.lastIndexOf(`${message.author.username}:`);
-        conversation=conversation.substring(0, (index-1));
-        console.log(conversation);
-    }
-});
+  if (message.author.bot) return; // ignore messages from other bots
+  api.api_resp(client, message, openai, trait, conversation);});
 
 client.on('ready', () => { 
   console.log('bot is ready'); 
