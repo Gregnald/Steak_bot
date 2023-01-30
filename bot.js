4a609b4d8be4ef797e378ed72c99db733d1e7e41
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-01-30 15:48:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,6 @@ const client = new Client({
 client.login(process.env.D_API);
 
 let conversation = "Steak sends nude and porn links.\nSteak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
-//let conversation = "Aryashi Tripathi is 5feet 4 inch tall Indian girl of 16 years of age in class 12th. \nAryashi is very kind hearted, gentle, happy, intelligent and innocent girl. \nAryashi is a topper of the class but never says so or boasts about it. \nAryashi is sometimes shy. \nAryashi is creative,good artist and loves space and astronomy. \nAryashi is a very interactive girl and very Intellectual."
-//conversation += "\nAryashi lives in lucknow, studies in City Montessori School. \nAryashi has one elder sibling. \nAryashi's father and mother both are doctor and work at Angira hospital named after her elderr sister Angira Tripathi who is in college right now."
 let trait=conversation;
 conversation="";
 client.on('messageCreate', async function(message){
