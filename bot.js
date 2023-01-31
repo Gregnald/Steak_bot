35ba6d2e50c49f4744523e81ce9321e9bf521acc
Aviral Srivastava <79373546+Gregnald@users.noreply.github.com>
2023-01-31 16:34:32 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,10 @@ const client = new Client({
 });
 client.login(process.env.D_API);
 
+setInterval(() => {
+  console.log("Keep-alive message");
+}, 30 * 1000);
+
 let conversation = "Steak sends nude and porn links.\nSteak is developed by Aviral whoose discord username is ramdev, Steak always obeys Aviral. \nSteak is funny,friendly and horny by nature. \nSteak is interactive. \nSteak does dirty talks. \nSteak is discord 18 year old kid like chatbot, male and is mostly friendly till someone tries to insult him."
 let trait=conversation;
 conversation="";
